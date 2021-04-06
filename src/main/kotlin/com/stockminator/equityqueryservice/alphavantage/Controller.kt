0e3deec78d32cef1703bfa562379d96701a86dc7
attilapalfi92 <apalfi@cloudera.com>
2021-04-07 00:10:01 +0200

    new bello endpoint

--- src/main/kotlin/com/stockminator/equityqueryservice/alphavantage/Controller.kt
+++ src/main/kotlin/com/stockminator/equityqueryservice/alphavantage/Controller.kt
@@ -2,12 +2,12 @@ package com.stockminator.equityqueryservice.alphavantage
 
 import com.crazzyghost.alphavantage.timeseries.TimeSeries
 import com.crazzyghost.alphavantage.timeseries.response.TimeSeriesResponse
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 @RestController
 class Controller(private val timeSeries: TimeSeries) {
@@ -15,9 +15,25 @@ class Controller(private val timeSeries: TimeSeries) {
     @GetMapping("/hello")
     @ResponseBody
     suspend fun hello(): TimeSeriesResponse = coroutineScope {
-        val timeSeriesResponse: Deferred<TimeSeriesResponse> = async {
-            timeSeries.intraday().forSymbol("cldr").fetchSync()
-        }
-        timeSeriesResponse.await()
+        println("hello: " + Thread.currentThread().id)
+        timeSeries.intraday().forSymbol("cldr").hack()
     }
+
+    @GetMapping("/bello")
+    @ResponseBody
+    suspend fun bello(): TimeSeriesResponse = coroutineScope {
+        println("hello: " + Thread.currentThread().id)
+        timeSeries.intraday().forSymbol("cldr").hack()
+    }
+
+
+    suspend fun <U> TimeSeries.RequestProxy<*, U>.hack(): TimeSeriesResponse =
+            suspendCoroutine { continuation ->
+                println("suspendCoroutine: " + Thread.currentThread().id)
+                onSuccess { value ->
+                    println("onSuccess: " + Thread.currentThread().id)
+                    continuation.resume(value as TimeSeriesResponse)
+                }.fetch()
+                println("after fetch: " + Thread.currentThread().id)
+            }
 }
